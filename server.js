8a75af41d9a07ce9c21d7aa187b33a226d260636
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2020-07-10 14:54:31 -0300

    Chat finished

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const path = require("path")
+const cors = require("cors")
+
 
 const app = express()
-const server = require('http').createServer(app)
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -14,6 +23,24 @@ app.use('/', (req,res) =>{
   res.render('index.html');
 })
 
-app.listen(3333, () => {
+let messages = []
+
+io.on('connection', socket => { 
+  console.log(`Socket conectado ${socket.id}`)
+
+
+  socket.emit('previousMessages', messages)
+
+  socket.on('sendMessage', data => {
+    messages.push(data) 
+    socket.broadcast.emit('receivedMessage', data)
+    
+  } 
+ )
+
+})
+
+
+server.listen(3333, () => {
   console.log("Server rodando...")
 })
