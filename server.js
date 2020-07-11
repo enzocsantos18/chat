c918cdf9d53588d924563512b80cb9de0f424110
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2020-07-11 16:18:18 -0300

    Design changes and chat improviments

--- server.js
+++ server.js
@@ -1,46 +1,42 @@
-const express = require('express')
-const path = require("path")
-const cors = require("cors")
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
 
-
-const app = express()
+const app = express();
 app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
-    next();
-});const server = require('http').createServer(app)
-const io = require('socket.io')(server)
+  //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+  res.header("Access-Control-Allow-Origin", "*");
+  //Quais são os métodos que a conexão pode realizar na API
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  app.use(cors());
+  next();
+});
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'public'))
-app.engine('html', require('ejs').renderFile)
-app.set('view engine', 'html')
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
-app.use('/', (req,res) =>{
-  res.render('index.html');
-})
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
 
-let messages = []
+app.use("/", (req, res) => {
+  res.render("index.html");
+});
 
-io.on('connection', socket => { 
-  console.log(`Socket conectado ${socket.id}`)
+let messages = [];
 
+io.on("connection", (socket) => {
+  console.log(`Socket conectado ${socket.id}`);
 
-  socket.emit('previousMessages', messages)
+  socket.emit("previousMessages", messages);
 
-  socket.on('sendMessage', data => {
-    messages.push(data) 
-    socket.broadcast.emit('receivedMessage', data)
-    
-  } 
- )
-
-})
-
+  socket.on("sendMessage", (data) => {
+    messages.push(data);
+    socket.broadcast.emit("receivedMessage", data);
+  });
+});
 
 server.listen(3333, () => {
-  console.log("Server rodando...")
-})
+  console.log("Server rodando...");
+});
